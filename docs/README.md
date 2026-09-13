eab02a797e58eb071fffb224f247545d09953621
Abdiaziz Ibrahim Kassim <abdiazizibrahim5909abc@gmail.com>
2026-09-13 14:38:54 +0300

    Week 1 assignment

--- docs/README.md
+++
@@ -1,1 +0,0 @@
-My Python Lab Project
---
+++ main.py
@@ -0,0 +1,9 @@
+# This program introduces me using variables.
+name = "Abdiaziz Ibrahim Kassim"
+age = 22
+favorite_language = "Python"
+goal = "To strengthen my python skills and build useful programs to solve problems"
+print("My name is: ",name)
+print("I am: ",age)
+print("My favourite programming language is: ",favorite_language)
+print("My goal for this course is: ",goal)
